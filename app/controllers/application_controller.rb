3a92cba5e40ba01e0d0dc2cd1be6f92405fc4b59
Chris Junker <elevatemedfit@gmail.com>
2019-11-18 16:31:09 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    @name = User.find(params[:name])
+    session[:name])
   end
 end
