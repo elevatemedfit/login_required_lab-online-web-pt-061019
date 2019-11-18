82b4d5ab25f0172b12518468dd9de3a99a15f9d8
Chris Junker <elevatemedfit@gmail.com>
2019-11-18 16:05:55 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,15 @@
 class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    if
+      params[:username].nil? or params[:username].empty?
+      redirect_to '/sessions/new'
+    else
+      session[:username] = params[:username]
+      redirect_to '/'
+  end
+  end
 end
