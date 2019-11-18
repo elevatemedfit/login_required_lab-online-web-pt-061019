106d662d367129e1087a12ccbbf5c341243eb139
Chris Junker <elevatemedfit@gmail.com>
2019-11-18 16:39:04 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,10 +1,18 @@
 class SecretsController < ApplicationController
+
+  before_action :require_login
+
   def show
     if current_user.include? :name
       render :show
     else
       redirect_to '/login'
     end
+  end
+
+  private
+  def require_login
+    return head(:forbidden) unless session.include? :name
   end
 
 end
