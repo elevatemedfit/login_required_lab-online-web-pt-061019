c8f9a9c25f02f7956065a628daa295573f46283a
Chris Junker <elevatemedfit@gmail.com>
2019-11-18 16:47:58 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get '/new' => 'sessions#new'
   post '/create' => 'sessions#create'
-  post '/logout' => 'sessions#destroy'
+  post '/destroy' => 'sessions#destroy'
   get '/show' => 'secrets#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
