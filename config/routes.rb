31a97ccbc0f270ae4ee1676fcd45cb0e523b6180
Chris Junker <elevatemedfit@gmail.com>
2019-11-18 16:47:20 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get '/home' => 'sessions/home'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/home' => 'sessions#home'
 
   get '/new' => 'sessions#new'
   post '/create' => 'sessions#create'
